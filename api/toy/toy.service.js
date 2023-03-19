727118b02c60d9c134eb3102609524e162697848
ilanSuli <ilansuli2147@gmail.com>
2023-03-19 07:26:46 +0200

    filter and users added

---
+++ api/toy/toy.service.js
@@ -0,0 +1,113 @@
+const dbService = require('../../services/db.service')
+const logger = require('../../services/logger.service')
+const utilService = require('../../services/util.service')
+const {ObjectId} = require('mongodb')
+
+async function query(filterBy={name:''}) {
+    try {
+        const criteria = {
+            name: { $regex: filterBy.name, $options: 'i' },
+            }
+        if(filterBy.labels.length){
+            criteria.labels = { $all: filterBy.labels } 
+        }
+        const collection = await dbService.getCollection('toy')
+        var toys = await collection.find(criteria)
+
+        if (filterBy.sortBy.by) {
+            const sortDirection = filterBy.sortBy.desc === 'true' ? 1 : -1;
+            const sortField = filterBy.sortBy.by;
+            toys = toys.sort({ [sortField]: sortDirection });
+          }
+        //    if (filterBy.labels.length) {
+        // toys = toys.filter(toy => {
+        //     return filterBy.labels.every(label => toy.labels.includes(label))
+        // })   
+    // }
+          toys = await toys.toArray()
+        return toys
+    } catch (err) {
+        logger.error('cannot find toys', err)
+        throw err
+    }
+}
+
+async function getById(toyId) {
+    try {
+        const collection = await dbService.getCollection('toy')
+        const toy = collection.findOne({ _id: new ObjectId(toyId) })
+        return toy
+    } catch (err) {
+        logger.error(`while finding toy ${toyId}`, err)
+        throw err
+    }
+}
+
+async function remove(toyId) {
+    try {
+        const collection = await dbService.getCollection('toy')
+        await collection.deleteOne({ _id: ObjectId(toyId) })
+    } catch (err) {
+        logger.error(`cannot remove toy ${toyId}`, err)
+        throw err
+    }
+}
+
+async function add(toy) {
+    try {
+        const collection = await dbService.getCollection('toy')
+        await collection.insertOne(toy)
+        return toy
+    } catch (err) {
+        logger.error('cannot insert toy', err)
+        throw err
+    }
+}
+
+async function update(toy) {
+    try {
+        const toyToSave = {
+            name: toy.name,
+            price: toy.price
+        }
+        const collection = await dbService.getCollection('toy')
+        await collection.updateOne({ _id: ObjectId(toy._id) }, { $set: toyToSave })
+        return toy
+    } catch (err) {
+        logger.error(`cannot update toy ${toyId}`, err)
+        throw err
+    }
+}
+
+async function addToyMsg(toyId, msg) {
+    try {
+        msg.id = utilService.makeId()
+        const collection = await dbService.getCollection('toy')
+        await collection.updateOne({ _id: ObjectId(toyId) }, { $push: { msgs: msg } })
+        return msg
+    } catch (err) {
+        logger.error(`cannot add toy msg ${toyId}`, err)
+        throw err
+    }
+}
+
+async function removeToyMsg(toyId, msgId) {
+    try {
+        const collection = await dbService.getCollection('toy')
+        await collection.updateOne({ _id: ObjectId(toyId) }, { $pull: { msgs: {id: msgId} } })
+        return msgId
+    } catch (err) {
+        logger.error(`cannot add toy msg ${toyId}`, err)
+        throw err
+    }
+}
+
+module.exports = {
+    remove,
+    query,
+    getById,
+    add,
+    update,
+    addToyMsg,
+    removeToyMsg
+}
